95c02baac9fd8c0873d25c2376d5cf5faf7dd8c4
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:31 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,27 +3,27 @@ from conans import ConanFile
 
 class BoostContainerConan(ConanFile):
     name = "Boost.Container"
-    version = "1.65.1"
+    version = "1.66.0"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Functional/1.65.1@bincrafters/testing", \
-        "Boost.Intrusive/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Functional/1.66.0@bincrafters/testing", \
+        "Boost.Intrusive/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing"
     lib_short_names = ["container"]
     is_header_only = False
 
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-container"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
